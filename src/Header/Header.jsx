acf905d3294816eecdcc745cdb24d0e87d770214
rizwan441 <rizwanzafar441@gmail.com>
2023-06-13 16:32:21 +0500

    first commit

---
+++ src/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Components/navBar/Navbar'
+
+const Header = () => {
+  return (
+      <div>
+          <Navbar />
+      </div>
+      
+  )
+}
+
+export default Header
